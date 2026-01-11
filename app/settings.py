808f26752ee41e5a363375e62e09aac5b2f53be6
yoga-ib <yogaselvaraj16@gmail.com>
2026-01-11 19:39:39 +0530

    Updated requirements

--- app/settings.py
+++ app/settings.py
@@ -8,6 +8,8 @@ os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
 HF_EMBED_MODEL = r"all-MiniLM-L6-v2"
 
+PINECONE_API_KEY = "pcsk_5YueeH_74nrFRY1FKSgN7KvoswoXHAUEU5qtcTDuuFfqYb5tbiMeTNC2fEmtCvvsCQibfK"
+
 PINECONE_HOST = "https://mini-ai-index-1dh48uz.svc.aped-4627-b74a.pinecone.io"
 PINECONE_INDEX_NAME = "mini-ai-index"
 
